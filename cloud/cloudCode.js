45ba5bbf3cbf8032d4d3db11eea25050e418685a
WuHaolin <gwuhaolin@gmail.com>
2015-04-16 14:48:51 +0800

    修改域名

--- cloud/cloudCode.js
+++ cloud/cloudCode.js
@@ -41,7 +41,7 @@ AV.Cloud.define('updateWechatMenu', function (request, response) {
             {
                 "type": "view",
                 "name": "书循",
-                "url": "http://ishuxun-wechat.avosapps.com/"
+                "url": "http://wechat.ishuxun.cn/"
             }
         ]
     };
