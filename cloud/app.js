c15528b52d97dde42127f5ebf2d5a6f15b57c933
WuHaolin <gwuhaolin@gmail.com>
2015-05-05 19:07:42 +0800

    优化主页

--- cloud/app.js
+++ cloud/app.js
@@ -8,6 +8,11 @@ var WechatMsg = require('cloud/wechatMsg.js');
 var app = Express();
 app.use(Express.compress(), null);//压缩返回的数据
 app.set('views', 'cloud/views');   // 设置模板目录
+app.set('view engine', 'jade');
 app.use(Express.bodyParser(), null);    // 读取请求 body 的中间件
 app.listen();
 app.use('/wechat/msg', WechatMsg.MsgHandler);//微信消息服务
+
+app.get('/test', function (req, res) {
+    res.render('test');
+});
