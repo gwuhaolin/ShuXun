b6a047c8ecec796f7656141738e6e50e22bc63f8
WuHaolin <gwuhaolin@gmail.com>
2015-07-14 11:03:00 +0800

    当没有书评时方便用户返回

--- web/wechat/js/controller/book_bookReview.js
+++ web/wechat/js/controller/book_bookReview.js
@@ -4,11 +4,14 @@
  */
 "use strict";
 
-APP.controller('book_bookReview', function ($scope, $stateParams, DoubanBook$) {
+APP.controller('book_bookReview', function ($scope, $stateParams, $ionicHistory, DoubanBook$) {
     $scope.BookReview = DoubanBook$.BookReview;
     $scope.BookReview.nowBookId = $stateParams['doubanBookId'];
     $scope.title = $stateParams['bookTitle'] + ' 书评';
     $scope.$on('$ionicView.afterLeave', function () {
         $scope.BookReview.clear();
     });
+    $scope.goBack= function () {
+        $ionicHistory.goBack();
+    }
 });
