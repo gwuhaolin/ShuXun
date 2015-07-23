034f10df366ec491eed2794abb79654a144088dc
WuHaolin <gwuhaolin@gmail.com>
2015-07-23 20:56:21 +0800

    为使用angular-material代替现有的semantic-ui做准备

---
+++ web/material/js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by wuhaolin on 7/23/15.
+ */
+var APP = angular.module('APP', ['ngMaterial']);
---
+++ web/material/js/controller/bookTagsContentCtrl.js
@@ -0,0 +1,6 @@
+/**
+ * Created by wuhaolin on 7/23/15.
+ */
+APP.controller('bookTagsContentCtrl', function ($scope,BookRecommend$) {
+    $scope.BookRecommend$ = BookRecommend$;
+});
