20a8f9bfec5c7351f441863335acdb3dfd1c233b
WuHaolin <gwuhaolin@gmail.com>
2015-07-24 22:23:05 +0800

    修改微信端在桌面浏览器显示时的鼠标样式适应桌面浏览

--- web/material/js/app.js
+++ web/material/js/app.js
@@ -1,4 +1,5 @@
 /**
  * Created by wuhaolin on 7/23/15.
  */
+var leanAnalytics = new _LeanAnalytics('Material');
 var APP = angular.module('APP', ['ngMaterial']);
--- web/material/js/controller/bookTagsContentCtrl.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by wuhaolin on 7/23/15.
- */
-APP.controller('bookTagsContentCtrl', function ($scope,BookRecommend$) {
-    $scope.BookRecommend$ = BookRecommend$;
-});
